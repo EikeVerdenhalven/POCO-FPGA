41f95d6057135993e978e437c64aac23ebca6bee
Eike Verdenhalven <eike.v@gmx.de>
2017-09-29 15:12:05 +0000

    fixed monitoring tool

--- hardware_monitor.py
+++ hardware_monitor.py
@@ -7,32 +7,57 @@ def format_entry(index, ratio):
     return "{0: >2}: {1: >5.1F}%".format(index, ratio)
 
 
-def print_rgb_led_data(screen, data, offset, size, initial_row=0, initial_col=0):
+def print_rgb_led_data(screen, data, offset, size, display_index_offset=0, initial_row=0, initial_col=0):
     row = initial_row
     index = offset
     col_offset = initial_col
+    display_index = display_index_offset
     while index < offset + size:
-        prefix = "{0: >3}: ".format(index)
+        prefix = "{0: >3}: ".format(display_index)
         screen.print_at(prefix, col_offset, row)
-        col = col_offset + len(prefix) + 2
+        col = col_offset + len(prefix)
         for color_letter in ['G', 'R', 'B']:
             s = format_entry(color_letter, data[index])
             index += 1
             screen.print_at(s, col, row)
             col += len(s) + 2
         row += 1
+        display_index += 1
         if row > screen.dimensions[0]:
             row = 0
             col_offset += 44
     return row, col_offset
 
+def print_linear_data(screen, data, offset, count, initial_row=0, initial_col=0):
+    index = offset
+    row = initial_row
+    col = 0
+    col_offset = initial_col
+    subindex = 0
+    while index < offset + count:
+        ds = "{0: >3}: {1: >5.1F}% ".format(index, data[index]);
+        screen.print_at(ds, col + col_offset, row)
+        col += len(ds)
+        subindex += 1
+        index += 1
+        if subindex == 3:
+            subindex = 0
+            row += 1
+            col = 0
+        if row == screen.dimensions[0]:
+            col_offset += 44
+            row = 0
+    return row, col
+
 
 def update_repl(screen):
     handle = POCOpif.initialize_pif()
+    screen.clear()
     for I in range(1, 100):
         data = POCOpif.get_LED_data(handle)
         last_position = print_rgb_led_data(screen, data, 0, 96)
-        last_position = print_rgb_led_data(screen, data, 112, 96, last_position[0], last_position[1])
+        last_position = print_rgb_led_data(screen, data, 112, 87, 32, last_position[0], last_position[1])
+        print_linear_data(screen, data, 224,112 , last_position[0], last_position[1])
         screen.refresh()
         sleep(0.5)
     POCOpif.deinit_pif(handle)
@@ -45,4 +70,5 @@ def test_hw():
 
 #test_hw()
 #print(len(POCOstub.get_LED_data()))
+
 Screen.wrapper(update_repl)
