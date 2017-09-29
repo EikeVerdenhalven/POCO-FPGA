bbced34b86e772d7b16e83ae8432b6a5e091d8f6
Eike Verdenhalven <eike.verdenhalven@native-instruments.de>
2017-09-29 15:23:48 +0200

    implemented screen printer for all channels

--- POCOstub.py
+++ POCOstub.py
@@ -2,6 +2,7 @@
 
 def get_LED_data():
     retval = []
-    for I in range(1, 336):
-        retval.append(0.5)
+    for chain in range(0, 3):
+        for I in range(0, 112):
+            retval.append(I * (1.0 / 112.0) * 100.0)
     return retval
--- hardware_monitor.py
+++ hardware_monitor.py
@@ -7,25 +7,31 @@ def format_entry(index, ratio):
     return "{0: >2}: {1: >5.1F}%".format(index, ratio)
 
 
-def print_led_data(screen, data):
+def print_rgb_led_data(screen, data, offset=0):
     row = 0
-    index = 0
-    while index < 96:
-        col = 0
-        for colidx in range(0, 3):
-            s = format_entry(index, data[index])
+    index = offset
+    col_offset = 0
+    while index < len(data):
+        prefix = "{0: >3}: ".format(index)
+        screen.print_at(prefix, col_offset, row)
+        col = col_offset + len(prefix) + 2
+        for color_letter in ['R', 'G', 'B']:
+            s = format_entry(color_letter, data[index])
             index += 1
             screen.print_at(s, col, row)
             col += len(s) + 2
         row += 1
+        if row > screen.dimensions[0]:
+            row = 0
+            col_offset += 46
 
 
 def update_repl(screen):
     for I in range(1, 100):
         data = POCOstub.get_LED_data()
-        print_led_data(screen, data)
+        print_rgb_led_data(screen, data, 0)
         screen.refresh()
         sleep(0.5)
 
-
+#print(len(POCOstub.get_LED_data()))
 Screen.wrapper(update_repl)
