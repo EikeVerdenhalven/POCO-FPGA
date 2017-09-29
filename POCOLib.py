d2a448b5d9e3d2ad1a75af8d342b1488fe255452
Eike Verdenhalven <eike.verdenhalven@native-instruments.de>
2017-09-29 18:25:59 +0200

    Fix HW lib

--- POCOLib.py
+++ POCOLib.py
@@ -1,24 +1,20 @@
 import POCOpif
 
 
-class POCOLib:
+class POCOLib(object):
 
     ROBOT_LIBRARY_SCOPE = 'TEST SUITE'
 
     def __init__(self):
         self.__POCOHANDLE__ = POCOpif.initialize_pif()
 
-    def _init_POCOhandle():
-        global POCOhandle
-        POCOhandle = POCOpif.initialize_pif()
+    def deinit(self):
+        if self.__POCOHANDLE__:
+            POCOpif.deinit_pif(self.__POCOHANDLE__)
+            self.__POCOHANDLE__ = None
 
-    def deinit():
-        global POCOhandle
-        if POCOhandle:
-            POCOpif.deinit_pif(POCOhandle)
-
-    def get_Keybed_RGB_LED(index):
-        data = POCOpif.get_LED_data(POCOhandle)
+    def get_Keybed_RGB_LED(self, index):
+        data = POCOpif.get_LED_data(self.__POCOHANDLE__)
         if index > 0 and index < 32:
             return data[index], data[index + 1], data[index + 2]
         else:
